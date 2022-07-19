a404ef861b1dc52b13dd027433bc51c8a7a4b34c
Chris Bata <cjbata@gmail.com>
2022-07-19 12:45:58 -0600

    add so much

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Chris Bata on 7/11/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
